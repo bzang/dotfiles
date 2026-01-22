c835b308313cc729fac6a12863b2b9df5e5c17ed
Bernie Zang <bernie.zang@classdojo.com>
2026-01-22 19:21:56 +0000

    Update gitconfig with user email and rebase settings; add new zsh aliases for iCloud and Obsidian; enhance zshrc with pyenv and pnpm configurations

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+icloud() { cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/"$1" }
+obsidian() { cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/"$1" }
